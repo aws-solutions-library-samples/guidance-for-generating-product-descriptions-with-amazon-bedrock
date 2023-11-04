924c715b3b319db95afb820571d418a35a9e915c
Kevin Bell <kebel@amazon.com>
2023-11-03 21:19:29 -0700

    Some folder restructuring

---
+++ source/frontend/src/config.js
@@ -0,0 +1,6 @@
+// begin generated code from update-config.sh
+export const AWS_REGION = 'us-west-2'
+export const USER_POOL_ID = 'us-west-2_SdzB7RSbc'
+export const USER_POOL_WEB_CLIENT_ID = '33h06gkmqpm3gs9dglateinmn'
+export const API_GATEWAY_URL = 'https://z2oq8k89rk.execute-api.us-west-2.amazonaws.com/prod/'
+// end generated code
